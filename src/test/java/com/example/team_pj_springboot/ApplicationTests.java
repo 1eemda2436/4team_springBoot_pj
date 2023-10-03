5a7c553816f5048d11f444f16f5ecc6b844fd729
ict02-23 <ict02-23@DESKTOP-4V8LP9S>
2023-10-03 19:36:43 +0900

    first commit

---
+++ src/test/java/com/example/team_pj_springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team_pj_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
