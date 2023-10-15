7315fd51fb25d7633c1cc8cd55cd7ea998d39d5c
LEESEOUNGMINv <always0616@gmail.com>
2023-10-15 22:07:29 +0900

    "PayStatement info"

--- src/main/java/com/example/team_pj_springboot/controller/SalaryController.java
+++ src/main/java/com/example/team_pj_springboot/controller/SalaryController.java
@@ -1,15 +1,21 @@
 package com.example.team_pj_springboot.controller;
 import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.team_pj_springboot.dto.MemberDTO;
 import com.example.team_pj_springboot.dto.SalaryAndMemberDTO;
+import com.example.team_pj_springboot.dto.SalaryDTO;
+import com.example.team_pj_springboot.dto.payStatementDTO;
 import com.example.team_pj_springboot.service.SalaryService;
 
 
@@ -23,6 +29,8 @@ public class SalaryController {
 	private SalaryService service;
 	
 	//http://localhost:8081/salary/salaryMain
+
+	//급여관리 메인 페이지
 	@GetMapping("/salaryMain")
     public List<SalaryAndMemberDTO> salaryAndMemberInfo() {
 		logger.info("[salaryAndMemberInfo]");
@@ -30,6 +38,17 @@ public class SalaryController {
         return service.getSalaryAndMemberInfo();
     }
 	
+	//급여 관리 명세서
+	@GetMapping("/PayStatement/{id}")
+	public Optional<payStatementDTO> payStatement(@PathVariable String id) {
+		logger.info("[payStatement]");
+
+		Optional<payStatementDTO> dto = service.payStatementInfo(id);
+		
+		return dto;
+	}
+	
+	//인사관리 메인 페이지
 	@GetMapping("/personnel")
 	public List<MemberDTO> personnelInfo(){
 		logger.info("[personnelInfo]");
--- src/main/java/com/example/team_pj_springboot/repository/SalaryRepository.java
+++ src/main/java/com/example/team_pj_springboot/repository/SalaryRepository.java
@@ -1,17 +1,26 @@
 package com.example.team_pj_springboot.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.team_pj_springboot.dto.SalaryAndMemberDTO;
 import com.example.team_pj_springboot.dto.SalaryDTO;
+import com.example.team_pj_springboot.dto.payStatementDTO;
 
 public interface SalaryRepository extends JpaRepository<SalaryDTO, Integer> {
 	@Query("SELECT new com.example.team_pj_springboot.dto.SalaryAndMemberDTO(s.s_id, m.id, m.name, m.resident, m.contract, m.depart_id, m.rank, m.state, m.e_state, s.salary) " +
 	        "FROM SalaryDTO s " +
 	        "JOIN MemberDTO m ON s.id = m.id")
 	List<SalaryAndMemberDTO> getSalaryAndMemberInfo();
-	
+
+	@Query("SELECT new com.example.team_pj_springboot.dto.payStatementDTO(s.s_id, m.id, m.name, m.rank, s.salary, s.income_tax, s.local_tax, s.national_pension, s.health_insurance, s.c_health_insurance, s.employment_insurance, s.bonus, s.allowance, s.overtime_pay, s.food_pay, s.t_pay, s.amount_money, s.amount_tax)"+
+		       "FROM SalaryDTO s " +
+		       "JOIN MemberDTO m ON s.id = m.id " + 
+		       "WHERE m.id = :memberId") // :memberId와 @Param("memberId")를 일치시킴
+	Optional<payStatementDTO> payStatementInfo(@Param("memberId") String memberId);
+
 }
