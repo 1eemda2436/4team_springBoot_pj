a9cb497a5aa1d2c5c383d2b88ac6fd63bb3287c3
yujindbwls <dladbwls1302@naver.com>
2023-10-23 11:54:09 +0900

    유진-departmentList수정중

---
+++ src/main/java/com/example/team_pj_springboot/service/DepartmentListService.java
@@ -0,0 +1,13 @@
+package com.example.team_pj_springboot.service;
+
+import java.util.List;
+
+import com.example.team_pj_springboot.dto.DepartmentListDTO;
+
+
+public interface DepartmentListService {
+   
+   public List<DepartmentListDTO> listAll();
+
+
+}
