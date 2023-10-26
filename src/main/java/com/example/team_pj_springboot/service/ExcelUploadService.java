38e693e1bf3439aa86eebddc0fd8bb8c093fcd2f
LEESEOUNGMINv <always0616@gmail.com>
2023-10-26 12:36:24 +0900

    "엑셀 업로드시 salary save"

--- src/main/java/com/example/team_pj_springboot/service/ExcelUploadService.java
+++ src/main/java/com/example/team_pj_springboot/service/ExcelUploadService.java
@@ -5,66 +5,113 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.team_pj_springboot.dto.PersonnelMemberDTO;
+import com.example.team_pj_springboot.dto.SalaryDTO;
 import com.example.team_pj_springboot.repository.PersonnelMemberRepository;
+import com.example.team_pj_springboot.repository.SalaryRepository;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.transaction.Transactional;
+
 @Service
 public class ExcelUploadService {
 
-    @Autowired
-    private PersonnelMemberRepository personnelMemberRepository;
+	@Autowired
+	private PersonnelMemberRepository personnelMemberRepository;
 
-//    public String uploadExcelData(MultipartFile file) {
-//        try {
-//            List<PersonnelMemberDTO> dataList = ExcelUtils.readExcelFile(file);
-//            
-//            // 데이터베이스에 저장
-//            personnelMemberRepository.saveAll(dataList);
-//            
-//            return "엑셀 파일 업로드 및 저장 성공!";
-//        } catch (Exception e) {
-//            return "엑셀 파일 업로드 및 저장 실패: " + e.getMessage();
-//        }
-//    }
-    
-    public String uploadExcelData(MultipartFile file) {
-        try {
-            List<PersonnelMemberDTO> dataList = ExcelUtils.readExcelFile(file);
+	@Autowired
+	private SalaryRepository salaryRepository;
 
-            // 중복 ID 확인 및 처리
-            List<String> duplicateIds = findDuplicateIds(dataList);
-            if (!duplicateIds.isEmpty()) {
-                // 중복 ID가 있는 경우 처리
-                return "중복된 ID가 있습니다: " + String.join(", ", duplicateIds);
-            }
+	//    public String uploadExcelData(MultipartFile file) {
+	//        try {
+	//            List<PersonnelMemberDTO> dataList = ExcelUtils.readExcelFile(file);
+	//
+	//            // 중복 ID 확인 및 처리
+	//            List<String> duplicateIds = findDuplicateIds(dataList);
+	//            if (!duplicateIds.isEmpty()) {
+	//                // 중복 ID가 있는 경우 처리
+	//                return "중복된 ID가 있습니다: " + String.join(", ", duplicateIds);
+	//            }
+	//
+	//            // 데이터베이스에 저장
+	//            personnelMemberRepository.saveAll(dataList);
+	//
+	//            return "엑셀 파일 업로드 및 저장 성공!";
+	//        } catch (Exception e) {
+	//            return "엑셀 파일 업로드 및 저장 실패: " + e.getMessage();
+	//        }
+	//    }
+	//    
+	//    //파일 업로드시 ID 중복 체크
+	//    private List<String> findDuplicateIds(List<PersonnelMemberDTO> dataList) {
+	//        List<String> duplicateIds = new ArrayList<>();
+	//        Set<String> uniqueIds = new HashSet<>();
+	//
+	//        for (PersonnelMemberDTO dto : dataList) {
+	//            String id = dto.getId();
+	//            if (uniqueIds.contains(id)) {
+	//                duplicateIds.add(id);
+	//            } else {
+	//                uniqueIds.add(id);
+	//            }
+	//        }
+	//
+	//        return duplicateIds;
+	//    }
 
-            // 데이터베이스에 저장
-            personnelMemberRepository.saveAll(dataList);
+	public String uploadExcelData(MultipartFile file) {
+		try {
+			List<PersonnelMemberDTO> dataList = ExcelUtils.readExcelFile(file);
 
-            return "엑셀 파일 업로드 및 저장 성공!";
-        } catch (Exception e) {
-            return "엑셀 파일 업로드 및 저장 실패: " + e.getMessage();
-        }
-    }
+			// 중복 ID 확인 및 처리
+			List<String> duplicateIds = findDuplicateIds(dataList);
+			if (!duplicateIds.isEmpty()) {
+				// 중복 ID가 있는 경우 처리
+				return "중복된 ID가 있습니다: " + String.join(", ", duplicateIds);
+			}
 
-    private List<String> findDuplicateIds(List<PersonnelMemberDTO> dataList) {
-        List<String> duplicateIds = new ArrayList<>();
-        Set<String> uniqueIds = new HashSet<>();
+			// 데이터베이스에 Personnel Member 정보 저장
+			personnelMemberRepository.saveAll(dataList);
 
-        for (PersonnelMemberDTO dto : dataList) {
-            String id = dto.getId();
-            if (uniqueIds.contains(id)) {
-                duplicateIds.add(id);
-            } else {
-                uniqueIds.add(id);
-            }
-        }
+			for (PersonnelMemberDTO dto : dataList) {
+				// Salary 정보를 저장
+				saveSalary(dto);
+			}
 
-        return duplicateIds;
-    }
-    
+			return "엑셀 파일 업로드 및 저장 성공!";
+		} catch (Exception e) {
+			return "엑셀 파일 업로드 및 저장 실패: " + e.getMessage();
+		}
+	}
+
+	// 파일 업로드시 ID 중복 체크
+	private List<String> findDuplicateIds(List<PersonnelMemberDTO> dataList) {
+		List<String> duplicateIds = new ArrayList<>();
+		Set<String> uniqueIds = new HashSet<>();
+
+		for (PersonnelMemberDTO dto : dataList) {
+			String id = dto.getId();
+			if (uniqueIds.contains(id)) {
+				duplicateIds.add(id);
+			} else {
+				uniqueIds.add(id);
+			}
+		}
+
+		return duplicateIds;
+	}
+
+	@Transactional
+	public SalaryDTO saveSalary(PersonnelMemberDTO dto) {
+		SalaryDTO sadto = new SalaryDTO();
+		sadto.setId(dto.getId());
+
+		SalaryDTO saveSalary = salaryRepository.save(sadto);
+
+		return saveSalary;
+	}
+
 }
