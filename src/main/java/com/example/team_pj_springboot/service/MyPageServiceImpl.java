450690edfb4f15931790b86cb27f58d582008213
yujindbwls <dladbwls1302@naver.com>
2023-10-30 14:27:20 +0900

    lyj commit

--- src/main/java/com/example/team_pj_springboot/service/MyPageServiceImpl.java
+++ src/main/java/com/example/team_pj_springboot/service/MyPageServiceImpl.java
@@ -2,15 +2,19 @@ package com.example.team_pj_springboot.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.team_pj_springboot.dto.AllPersonnelDTO;
 import com.example.team_pj_springboot.dto.MemberDTO;
 import com.example.team_pj_springboot.dto.MyPageDTO;
+import com.example.team_pj_springboot.dto.UserRoleDTO;
 import com.example.team_pj_springboot.mappers.PersonnelMapper;
 import com.example.team_pj_springboot.repository.MemberRepository;
 import com.example.team_pj_springboot.repository.MyPageRepository;
+import com.example.team_pj_springboot.repository.UserRoleRepository;
 
 @Service
 public class MyPageServiceImpl implements MyPageService{
@@ -19,10 +23,11 @@ public class MyPageServiceImpl implements MyPageService{
 //	private MyPageRepository dao;
 	
 	@Autowired
-	private MemberRepository dao1;
+	private MemberRepository member;
 	
 	@Autowired
 	private PersonnelMapper mapper;
+	
 	
 
 //	@Override
@@ -47,7 +52,10 @@ public class MyPageServiceImpl implements MyPageService{
 	
 	// 마이페이지 수정
 	@Override
-	public void save(MemberDTO member) {
-		dao1.save(member);
+	public void save(MemberDTO dto) {
+		System.out.println("수정");
+		member.updateMember(dto.getName(), dto.getEmail(), dto.getTel(), dto.getBank());
 	}
+	
+	
 }
