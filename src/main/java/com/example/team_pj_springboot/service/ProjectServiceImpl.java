45e1f2ed15e99fafff417699d1bb1f6093fb7ac9
1eemda_owo <135786989+1eemda2436@users.noreply.github.com>
2023-10-30 14:24:28 +0900

    프로젝트 워크스페이스 수정 1차

--- src/main/java/com/example/team_pj_springboot/service/ProjectServiceImpl.java
+++ src/main/java/com/example/team_pj_springboot/service/ProjectServiceImpl.java
@@ -1,17 +1,11 @@
 package com.example.team_pj_springboot.service;
 
-import java.io.IOException;
 import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 import com.example.team_pj_springboot.repository.ProjectRepository;
-import com.example.team_pj_springboot.dto.ProjectAndTeamDTO;
 import com.example.team_pj_springboot.dto.ProjectDTO;
 
 @Service
@@ -19,32 +13,55 @@ public class ProjectServiceImpl implements ProjectService {
 
 	@Autowired
 	private ProjectRepository dao;
-	
-	// List
+
 	@Override
-	public List<ProjectDTO> listAll(HttpServletRequest req, Model model) 
-			throws ServletException, IOException {
-		return dao.findAll();
+	public List<ProjectDTO> listAll(int team_id) {
+		// TODO Auto-generated method stub
+		return dao.findProject(team_id);
 	}
 
-	// Insert : PK가 존재하지 않을때는 신규, PK가 존재할 때는 해당키의 데이터를 update
 	@Override
-	public void save(ProjectDTO dto)
-			throws ServletException, IOException {
+	public void save(ProjectDTO dto) {
+		// TODO Auto-generated method stub
 		dao.save(dto);
 	}
 
 	@Override
-	public ProjectDTO get(int pj_id) 
-			throws ServletException, IOException {
+	public ProjectDTO get(int pj_id) {
+		// TODO Auto-generated method stub
 		return dao.findById(pj_id).get();
 	}
 
 	@Override
-	public void delete(int pj_id)
-			throws ServletException, IOException {
+	public void delete(int pj_id) {
+		// TODO Auto-generated method stub
 		dao.deleteById(pj_id);
-		
 	}
+	
+//	// List
+//	@Override
+//	public List<ProjectDTO> listAll(HttpServletRequest req, Model model) {
+//		return dao.findAll();
+//	}
+//
+//	// Insert : PK가 존재하지 않을때는 신규, PK가 존재할 때는 해당키의 데이터를 update
+//	@Override
+//	public void save(ProjectDTO dto)
+//			throws ServletException, IOException {
+//		dao.save(dto);
+//	}
+//
+//	@Override
+//	public ProjectDTO get(int pj_id) 
+//			throws ServletException, IOException {
+//		return dao.findById(pj_id).get();
+//	}
+//
+//	@Override
+//	public void delete(int pj_id)
+//			throws ServletException, IOException {
+//		dao.deleteById(pj_id);
+//		
+//	}
 
 }
--- src/main/java/com/example/team_pj_springboot/service/ProjectWorkServiceImpl.java
+++ src/main/java/com/example/team_pj_springboot/service/ProjectWorkServiceImpl.java
@@ -1,14 +1,9 @@
 package com.example.team_pj_springboot.service;
 
-import java.io.IOException;
 import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 import com.example.team_pj_springboot.repository.ProjectWorkRepository;
 import com.example.team_pj_springboot.dto.ProjectWorkDTO;
@@ -18,30 +13,61 @@ public class ProjectWorkServiceImpl implements ProjectWorkService {
 
 	@Autowired
 	private ProjectWorkRepository dao;
+
+	@Override
+	public List<ProjectWorkDTO> list(int pj_id) {
+		// TODO Auto-generated method stub
+		return dao.findProjectWork(pj_id);
+	}
+
+	@Override
+	public void save(ProjectWorkDTO dto) {
+		// TODO Auto-generated method stub
+		dao.save(dto);
+	}
+
+	@Override
+	public void saveComplete(int pw_id, String complete) {
+		// TODO Auto-generated method stub
+		dao.updateComplete(pw_id, complete);
+	}
 	
 	@Override
-	public List<ProjectWorkDTO> listAll(HttpServletRequest req, Model model) 
-			throws ServletException, IOException {
-		return dao.findAll();
-	}
-
-	@Override
-	public void save(ProjectWorkDTO dto) 
-			throws ServletException, IOException {
-		dao.save(dto);
-		
-	}
-
-	@Override
-	public ProjectWorkDTO get(int pw_id) 
-			throws ServletException, IOException {
+	public ProjectWorkDTO get(int pw_id) {
+		// TODO Auto-generated method stub
 		return dao.findById(pw_id).get();
 	}
 
 	@Override
-	public void delete(int pw_id) 
-			throws ServletException, IOException {
+	public void delete(int pw_id) {
+		// TODO Auto-generated method stub
 		dao.deleteById(pw_id);
 	}
+
+	
+//	@Override
+//	public List<ProjectWorkDTO> listAll(HttpServletRequest req, Model model) 
+//			throws ServletException, IOException {
+//		return dao.findAll();
+//	}
+//
+//	@Override
+//	public void save(ProjectWorkDTO dto) 
+//			throws ServletException, IOException {
+//		dao.save(dto);
+//		
+//	}
+//
+//	@Override
+//	public ProjectWorkDTO get(int pw_id) 
+//			throws ServletException, IOException {
+//		return dao.findById(pw_id).get();
+//	}
+//
+//	@Override
+//	public void delete(int pw_id) 
+//			throws ServletException, IOException {
+//		dao.deleteById(pw_id);
+//	}
 
 }
