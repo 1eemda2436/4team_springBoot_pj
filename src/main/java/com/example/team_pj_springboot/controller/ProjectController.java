37f3a4b9038567d9b311854379f83f19c0496047
LEESEOUNGMINv <always0616@gmail.com>
2023-10-31 09:55:52 +0900

    lsm ss

--- src/main/java/com/example/team_pj_springboot/controller/ProjectController.java
+++ src/main/java/com/example/team_pj_springboot/controller/ProjectController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,49 +24,52 @@ import com.example.team_pj_springboot.service.ProjectServiceImpl;
 @RestController
 @RequestMapping("/guest/project")
 public class ProjectController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(ProjectController.class);
-	
-	@Autowired
-	private ProjectServiceImpl service;
-	
-	// 프로젝트 리스트
-	@GetMapping("/list/{team_id}")
-	public List<ProjectDTO> ProjectList(@PathVariable(name="team_id")int team_id)
-			throws ServerException, IOException {
-		
-		return service.listAll(team_id);
-	}
-	
-	// 프로젝트 생성
-	@PostMapping("/pjadd")
-	public void save(@RequestBody ProjectDTO dto) 
-			throws ServerException, IOException {
-		
-		System.out.println(dto);
-		
-		service.save(dto);
-	}
-	
-	// 프로젝트 1건
-	@GetMapping("/{pj_id}")
-	public ProjectDTO fetchProjectById(@PathVariable(name="pj_id")int pj_id) 
-			throws ServerException, IOException {
-		
-		ProjectDTO dto = service.get(pj_id);
-		return dto;
+   
+   private static final Logger logger = LoggerFactory.getLogger(ProjectController.class);
+   
+   @Autowired
+   private ProjectServiceImpl service;
+   
+   // 프로젝트 리스트
+   @GetMapping("/list/{team_id}")
+   public List<ProjectDTO> ProjectList(@PathVariable(name="team_id")int team_id)
+         throws ServerException, IOException {
+      
+      return service.listAll(team_id);
+   }
+   
+   // 프로젝트 생성
+   @PostMapping("/pjadd")
+   public void save(@RequestBody ProjectDTO dto) 
+         throws ServerException, IOException {
+	   dto.setShow("Y");
+      System.out.println(dto);
+      service.save(dto);
+   }
+   
+   // 프로젝트 1건
+   @GetMapping("/{pj_id}")
+   public ProjectDTO fetchProjectById(@PathVariable(name="pj_id")int pj_id) 
+         throws ServerException, IOException {
+      
+      ProjectDTO dto = service.get(pj_id);
+      return dto;
 
-	}
-	
-	// 프로젝트 삭제
-	@DeleteMapping("/{pj_id}")
-	public void ProjectWorkDelete(@PathVariable(name="pj_id") int pj_id) 
-			throws ServerException, IOException {
-		
-		service.delete(pj_id);
-	}
+   }
+   
+   // 프로젝트 삭제
+   @DeleteMapping("/{pj_id}")
+   public void ProjectWorkDelete(@PathVariable(name="pj_id") int pj_id) 
+         throws ServerException, IOException {
+      
+      service.delete(pj_id);
+   }
 
-	
-	
+   // 프로젝트 업데이트 show
+   @PutMapping("/update/{pj_id}")
+   public void projectShow(@PathVariable(name="pj_id")int pj_id) {
+      service.updateShow(pj_id);
+   }
+   
 
 }
